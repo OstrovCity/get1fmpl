e7408506d6c591a4de75148ae79ca1aeda46fbe7
OstrovCity <50834214+OstrovCity@users.noreply.github.com>
2021-07-06 16:22:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,61 @@
 from io import open as io_open
+import sys
+import getopt
 import requests
 
-ICECAST_URL = 'http://prmstrm.1.fm:8000/'
-# ICECAST_URL = 'http://strm112.1.fm/'
 CHANNELS_API_URL = 'http://androidapp.1.fm/stlistandroid'
+premium = False
+out_file = '1fm.m3u'
 
+def parse_cl(argv):
+
+	help_str = sys.argv[0] + ' -p -f <output_filename>'
+	try:
+		opts, args = getopt.getopt(argv, 'hpf:', ['cl_file='])
+	except getopt.GetOptError:
+		print(help_str)
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print(help_str)
+			sys.exit()
+		elif opt == '-p':
+			global premium
+			premium = True
+		elif opt in ('-f', '--cl_file'):
+			global out_file
+			out_file = arg
+		else:
+			sys.exit(2)
 
 def main():
-    resp = requests.get(CHANNELS_API_URL)
-    channels = resp.json()
-    channels_sorted = sorted(channels, key=lambda x: x['StationName'].lower())
-    with io_open('1fm.m3u', 'w', encoding='utf8') as f:
-        f.write("#EXTM3U\n")
-        f.write("#PLAYLIST:1.fm\n")
-        for channel in channels_sorted:
-            station_name = channel['StationName']
-            station_desc = channel['StationDesc'].replace('\r\n', '').replace('&amp;', '&').replace('  ', ' ')
-            name = f"{station_name} - {station_desc}"
-            f.write(f"#EXTINF:-1,{name[:256]}\n")
-            f.write(f"{ICECAST_URL}{channel['Stream128k'].split('/')[-1].split('_mobile_mp3')[0]}\n")
-    print(len(channels_sorted))
+
+	parse_cl(sys.argv[1:])
+
+	if premium:
+		ICECAST_URL = 'http://prmstrm.1.fm:8000/'
+		quality = 'premium'
+	else:
+		ICECAST_URL = 'http://strm112.1.fm/'
+		quality = 'standart'   
+
+	print('quality ' + quality)
+	print('file    ' + out_file)
+
+	resp = requests.get(CHANNELS_API_URL)
+	channels = resp.json()
+	channels_sorted = sorted(channels, key=lambda x: x['StationName'].lower())
+	print(out_file)
+	with io_open(out_file, 'w', encoding='utf8') as f:
+		f.write("#EXTM3U\n")
+		f.write("#PLAYLIST:1.fm\n")
+		for channel in channels_sorted:
+			station_name = channel['StationName']
+			station_desc = channel['StationDesc'].replace('\r\n', '').replace('&amp;', '&').replace('  ', ' ')
+			name = f"{station_name} - {station_desc}"
+			f.write(f"#EXTINF:-1,{name[:256]}\n")
+			f.write(f"{ICECAST_URL}{channel['Stream128k'].split('/')[-1].split('_mobile_mp3')[0]}\n")
+	print(len(channels_sorted))
 
 if __name__ == '__main__':
-    main()
+	main()
